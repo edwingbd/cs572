6e6bb53bbd36b2d172f917cd11227ff8cdbb374a
edwingbd <edwingbd@gmail.com>
2019-04-01 16:17:18 -0500

    Update Exercise1.js
    test1

---
+++ homework_01/Exercise1.js
@@ -0,0 +1,18 @@
+//function that receive 
+function remove(arraytext){
+	log.console(this);
+}
+
+function otro(test){
+	test.removestr=remove();
+  log.console(this);
+}
+
+//__proto__.filterWords=function( arrayss){
+  //return "popo";
+//}
+let test=console.log.bind();
+test("mira");
+
+console.log("test");
+console.log("asdf asdf cosa asdf".filterWords(['asdf','cosa','noesta' ]) );

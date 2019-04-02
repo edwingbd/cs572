de872185d8d742120c45f72028a9ecc835dc1bab
edwingbd <edwingbd@gmail.com>
2019-04-02 16:59:26 -0500

    update for the h1 and h2

--- homework_01/Exercise1_d.js
+++ homework_01/Exercise1_d.js
@@ -15,29 +15,3 @@ var subscription = obs.subscribe(
     function (x) { console.log(`Value: ${x}`); },
     function (err) { console.error(err); },
     function () { console.info(`Done`); });
-
-
-const promiseFun = async function(array_2){
-        try{
-            return new Promise((resolve, reject)=>{
-            const textArray = this.split(" ")
-                                    .map(e=>{
-                                    if(array_2.find(b=>b===e,false))
-                                        return "*****";
-                                    else
-                                        return e;
-                                    }).reduce((j,k)=>j+" "+k,"") ;
-
-            resolve( textArray);
-        })
-        }
-        catch (error){
-            reject("something happens error="+error);
-        }
-    }
-
-    String.prototype.filterwords= promiseFun;
-console.log("usin Observable");
-console.log("step1");
-"this house is nice".filterwords( ['house','nice']).then(result => console.log(result));
-console.log("step2");

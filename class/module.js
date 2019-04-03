c27aedfb027e37379fab0130db55d481d15c9caa
edwingbd <edwingbd@gmail.com>
2019-04-03 14:08:36 -0500

    finish homework 3
    i did but i need to validate with the professor if it is correct

---
+++ class/module.js
@@ -0,0 +1,6 @@
+var x = 5;
+function y(){
+console.log('y funcitons')    
+}
+
+module.exports = {x:x,y:y}
--- homework_02/Exercise_2.js
+++ homework_02/Exercise_2.js
@@ -1,15 +1,8 @@
-const promiseFun = async function(array_2){
+const evenPro = async function(array_2){
     try{
-        return new Promise((resolve, reject)=>{
-        const textArray = this.split(" ")
-                                .map(e=>{
-                                if(array_2.find(b=>b===e,false))
-                                    return "*****";
-                                else
-                                    return e;
-                                }).reduce((j,k)=>j+" "+k,"") ;
-
-        resolve( textArray);
+        return await new Promise((resolve, reject)=>{
+            const procededArray = this.filter(e=>!(e %2 ===0));
+        resolve(procededArray);
     })
     }
     catch (error){
@@ -17,13 +10,30 @@ const promiseFun = async function(array_2){
     }
 }
 
-String.prototype.filterwords= promiseFun;
-console.log("usin async/await");
-console.log("step1");
-"this house is nice".filterwords( ['house','nice']).then(result => console.log(result));
-console.log("step2");
+const oddPro = async function(){
+    try{
+        return new Promise((resolve, reject)=>{
+        const procededArray = this.filter(e=>(e %2 ===0));
+        resolve(procededArray);
+    })
+    }
+    catch (error){
+        reject("something happens error="+error);
+    }
+}
+
+
+// String.prototype.filterwords= promiseFun;
+
+// console.log("usin async/await");
+// console.log("step1");
+// "this house is nice".filterwords( ['house','nice']).then(result => console.log(result));
+// console.log("step2");
+
+Array.prototype.even=evenPro;
+Array.prototype.odd=oddPro;
 
 console.log('start');
-[1,2,3,4,5,6,7,8].even();
-[1,2,3,4,5,6,7,8].odd();
+[1,2,3,4,5,6,7,8].even().then(result => console.log(result));
+[1,2,3,4,5,6,7,8].odd().then(result => console.log(result));
 console.log('end')

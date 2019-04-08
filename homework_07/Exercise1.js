ef3511b5c98c88b54af9fca2b7731b441c8f7f1a
edwingbd <edwingbd@gmail.com>
2019-04-08 14:02:48 -0500

    lkab

---
+++ homework_07/Exercise1.js
@@ -0,0 +1,17 @@
+//Exercise 1 Schema Design 
+//Create a NoSQL design model for an application to manage a library, taking into consideration the following requirements:
+// • Books have an ISBN number and are categorized by author and tagged by keywords to facilitate search
+// • Books can be borrowed by students, so the librarian will be able to check all borrowed books and their return date so he 
+//      may contact students who are late returning their books. 
+
+const collecBooks={
+    ISBN:"123456",
+    inventoryCode:'456-123',
+    author:[{name:'edwin',age:89},{name:'mendizajo',age:Infinity}],
+    title:'text',
+    tags:['word1','word2'],
+    isborrow:true,
+    studentBorrow:{studenID:986924,phone:456789123,email:'edwun@EADDRINUSE.com'},
+    dateStartBorrow:'04/01/2019',
+    dateReturnBorrow:'04/31/2019'
+}
---
+++ homework_07/Exercise2.js
@@ -0,0 +1,3 @@
+// Exercise Reveal The Secret Message! I 
+//  1. Using DaaS, read one document from: DB: homeworkel, Collection: data, username: homeworkOl, password: homeworkel Connection String: mongodb://homeworkel:homeworkel@ds233806.mlab.com:33806/homeworkel 
+// 1. Write Express application and use findOne() with projection to read two fields {key, message} to read the document from mLab Server, decrypt the message using ('simple-encryptor') 2. Send the decrypted message as JSON object in the response to a route (/secret). 3. Upload your code to Github (without dependencies). 

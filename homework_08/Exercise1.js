9570d558aaa06457e33b3bf3dda7d0684b692401
edwingbd <edwingbd@gmail.com>
2019-04-09 14:00:12 -0500

    Create Exercise1.js

---
+++ homework_08/Exercise1.js
@@ -0,0 +1,81 @@
+// Questions
+
+// 1. Write a MongoDB query to display all the documents in the collection
+// restaurants.
+
+// 2. Write a MongoDB query to display the fields restaurant_id, name, district and
+// cuisine for all the documents in the collection restaurant.
+
+// 3. Write a MongoDB query to display the fields restaurant_id, name, district and
+// cuisine, but exclude the field _id for all the documents in the collection
+// restaurant.
+
+// 4. Write a MongoDB query to display the fields restaurant_id, name, district and
+// zipcode, but exclude the field _id for all the documents in the collection
+// restaurant.
+
+// 5. Write a MongoDB query to display all the restaurant which is in the district
+// Bronx.
+// 6. Write a MongoDB query to display the first 5 restaurant which is in
+// the district Bronx.
+
+// 7. Write a MongoDB query to display the next 5 restaurants after
+// skipping first 5 which are in the district Bronx.
+
+// 8. Write a MongoDB query to find the restaurants which locates in
+// coord value less than -95.754168.
+
+// 9. Write a MongoDB query to find the restaurants that does not
+// prepare any cuisine of ‘American’ and their grade score more than
+// 70 and coord value less than -65.754168.
+
+// 10. Write a MongoDB query to find the restaurant Id, name, district and
+// cuisine for those restaurants which contains 'Wil' as first three
+// letters for its name.
+
+// 11. Write a MongoDB query to find the restaurant Id, name, district and
+// cuisine for those restaurants which contains ‘ces' as last three
+// letters for its name.
+
+// 12. Write a MongoDB query to find the restaurant Id, name, district and
+// cuisine for those restaurants which contains ‘Reg’ as three letters
+// somewhere in its name.
+
+// 13. Write a MongoDB query to find the restaurants which belongs to
+// the district Bronx and prepared either American or Chinese dish.
+
+// 14. Write a MongoDB query to find the restaurant Id, name, district and
+// cuisine for those restaurants which belongs to the district Staten
+// Island or Queens or Bronx or Brooklyn.
+
+// 15. Write a MongoDB query to find the restaurant Id, name, district and
+// cuisine for those restaurants which are not belonging to the district
+// Staten Island or Queens or Bronx or Brooklyn.
+
+// 16. Write a MongoDB query to find the restaurant Id, name, district and
+// cuisine for those restaurants which achieved a score which is not
+// more than 10.
+
+// 17. Write a MongoDB query to find the restaurant Id, name, address
+// and geographical location for those restaurants where 2nd element
+// of coord array contains a value which is more than 42 and up to 52.
+
+// 18. Write a MongoDB query to arrange the name of the restaurants in
+// ascending order along with all the columns.
+
+// 19. Write a MongoDB query to arrange the name of the restaurants in
+// descending order along with all the columns.
+
+// 20. Write a MongoDB query to arrange the name of the cuisine in
+// ascending order and for those same cuisine district should be in
+// descending order.
+
+// 21. Write a MongoDB query to know whether all the addresses
+// contains the street or not.
+
+// 22. Write a MongoDB query which will select all documents in the
+// restaurants collection where the coord field value is Double.
+
+// 23. Write a MongoDB query to find the restaurant name, district,
+// longitude and latitude and cuisine for those restaurants which
+// contains 'Mad' as first three letters of its name.

8f35ed50e0424971297681ac1237f7217558e9b8
edwingbd <edwingbd@gmail.com>
2019-04-10 14:54:30 -0500

    Update Exercise.js

--- homework_09/Exercise.js
+++ homework_09/Exercise.js
@@ -12,12 +12,18 @@ show collections
 // your MongoDB (mongoimport).
 // Use the Aggregation Framework to write 4 pipelines:
 // 1. Find all the zip codes in Washington state.
-db.zips.aggregate([{$match:{state:'WA'}}])
+db.zips.aggregate([{$match:{state:'WA'}},{$project:{_id:0,zip_Code:"$_id"}}])
 // 2. Find all the zip codes with a population less than 5000.
-db.zips.aggregate([{$group:{_id:'$_id',population:{$max:'$pop'}}},{$match:{population:{$gte:5000}}},{$project:{_id:0,zip_Code:"$_id",city:"$population"}}])
+db.zips.aggregate([{$match:{'pop':{$lt:5000}}},{$project:{_id:0,zip_Code:"$_id",population:"$pop"}}])
 // 3. Find all cities that have more than one zip code, sort the results by state and city name.
 //// paso 1
 db.zips.aggregate([{$group:{_id:{state:'$state',city:'$city'},quantity:{$sum:1}}},{$project:{City:'$city'}}])
 //// paso 2
 db.zips.aggregate([{$group:{_id:{state:'$state',city:'$city'},quantity:{$sum:1}}},{$match:{quantity:{$gt:1}}},{$project:{_id:0,City:'$_id.city',Quantity:'$quantity'}}])
 // 4. Display the least populated city in each state
+
+db.zips.aggregate([
+    {$group:{_id:{state:'$state',city:'$city'},population:{$sum:'pop'}}},
+    {$sort:{state:1, population:1}},
+    {$group:{_id:{state:'$_id.state'},population2:{$first:'$population'}},city:{$first:'$_id.city'}},
+    {$project:{_id:1,population:'$population2'}}])

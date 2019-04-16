2fc2d9b5644cefe4e37881010553bf9af0f3d023
edwingbd <edwingbd@gmail.com>
2019-04-15 20:02:09 -0500

    homework

---
+++ Homework_11/app.js
@@ -0,0 +1,6 @@
+var var1 = 10;
+var var2 = 30;
+function suma(ab1, ab2) {
+    return ab1 + ab2;
+}
+console.log(suma(var1, var2));
---
+++ Homework_11/app.ts
@@ -0,0 +1,8 @@
+let var1 =10;
+let var2 =30;
+
+function suma (ab1,ab2){
+    return ab1+ ab2;
+}
+
+console.log(suma(var1,var2));
